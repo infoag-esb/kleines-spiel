ace82cae0acdcaff0a7b7df40b75bf921a47bd00
MichaelKutowski <Michael.Kutowski1@gmail.com>
2016-12-09 00:58:21 +0100

    Main master class - Kommentar verbesserung Michaels eigene Klasse für SoundTesten

--- InfoAG-Schulprojekt/src/MMC/Main.java
+++ InfoAG-Schulprojekt/src/MMC/Main.java
@@ -13,18 +13,18 @@ import java.awt.event.ActionListener;
 public class Main extends JFrame implements ActionListener	
 {
 	private static final long serialVersionUID = 1L;	//default warnung nullen
-	JButton testButton = new JButton("test");		//Button mit Text "test"
-	JLabel testLabel = new JLabel();		//label komponent erstellt	
+	JButton testButton = new JButton("test");			//Button mit Text "test"
+	JLabel testLabel = new JLabel();					//label komponent erstellt	
 	
 	public Main()
 	{
-		this.setBounds(100, 100, 300, 300);		//frame setzt position und größe vom fenster
-		this.setTitle("InfoAG - master");		//titel für das Fenster
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);		//Default close
+		this.setBounds(100, 100, 300, 300);				//frame setzt position und größe vom fenster
+		this.setTitle("InfoAG - master");				//titel für das Fenster
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);	//Default close
 		this.setLayout(null);		//Layout null, müssen size von koponenten selbst setzen
 	
 		testButton.setBounds(0, 0, 100, 100);		//position und Größe vom Button gesetzt
-		testButton.addActionListener(this);		//listener hinzugefügt
+		testButton.addActionListener(this);			//listener hinzugefügt
 		
 		testLabel.setBounds(100, 0, 100, 100);		//pos + size 
 		testLabel.setText("einfacher Label");		//text vom label gesetzt
@@ -35,8 +35,8 @@ public class Main extends JFrame implements ActionListener
 	
 	public static void main(String[] args)
 	{
-		Main mainheader = new Main();		//Main() wird erstellt 
-		mainheader.setVisible(true);		//mainheader wird sichtbar
+		Main mainheader = new Main();			//Main() wird erstellt 
+		mainheader.setVisible(true);			//mainheader wird sichtbar
 		
 		System.out.println("Moin InfoAG!");		//test print
 	}
---
+++ InfoAG-Schulprojekt/src/MMC/MichaelSoundKlasse.java
@@ -0,0 +1,20 @@
+/*
+ * Michael Kutowski Sound Klasse. Ziel .wav Sample Output als audio in jeglichen mixer output
+ * z.B. Headphones oder Lautsprecher
+ */
+
+package MMC;
+
+public class MichaelSoundKlasse
+{
+	//ich vermeide viele static änderungen durch public MichaelSoundKlasse() - 
+	public MichaelSoundKlasse()
+	{
+		System.out.println("asd");
+	}
+	
+	public static void main(String[] args)
+	{
+		MichaelSoundKlasse MSK = new MichaelSoundKlasse();
+	}
+}
