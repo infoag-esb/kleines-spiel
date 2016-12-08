fa5ec550c98830af28404f94d08bf34800b341cb
MichaelKutowski <Michael.Kutowski1@gmail.com>
2016-12-08 19:39:28 +0100

    Der Commit Anfang für das Anfang Programm - extends ein JFrame und implemented einen Actionlistener für den Button aus swing

---
+++ InfoAG-Schulprojekt/src/MMC/Main.java
@@ -0,0 +1,46 @@
+/*
+ InfoAG-Schulprojekt. Anfang erstellt von Michael Kutowski
+ 
+ master enthält nur eine Anfangsklasse die Text ausgibt zum Testen
+ */
+
+package MMC;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/* Main Klasse für eine einfache Frame Erstellung zum Testen von GitHub */
+public class Main extends JFrame implements ActionListener	
+{
+	private static final long serialVersionUID = 1L;	//default warnung nullen
+	JButton testButton = new JButton("test");
+	
+	public Main()
+	{
+		this.setBounds(100, 100, 300, 300);				//frame setzt position und größe vom fenster
+		this.setTitle("InfoAG - master");				//titel für das Fenster
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);	//Default close
+		this.setLayout(null);							//Layout null, müssen size von koponenten selbst setzen
+	
+		testButton.setBounds(0, 0, 100, 100);			//position und Größe vom Button gesetzt
+		testButton.addActionListener(this);				//listener hinzugefügt
+		
+		this.add(testButton);							//button komponent wird zum frame geadded
+	}
+	
+	public static void main(String[] args)
+	{
+		Main mainheader = new Main();					//Main() wird erstellt 
+		mainheader.setVisible(true);					//mainheader wird sichtbar
+		
+		System.out.println("Moin InfoAG!");				//test print
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) 
+	{
+		if(event.getSource() == this.testButton)		//wenn actionevent == vom testbutton
+			System.out.println("wurde Gedrückt");		//test print
+	}
+}
